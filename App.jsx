8b31c91af82f9188b7519c010d8caba7fe9ceab0
fersonull <culaspero@gmail.com>
2026-01-07 18:59:29 +0800

    environment setup

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "./global.css"
+
+export default function App() {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-2xl font-bold'>You are now set!</Text>
+    </View>
+  )
+}
